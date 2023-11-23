6a4dcd2642083ea439d48b39031f47b07f2ffb39
Aarti Bhagat <123726439+AartiPBhagat@users.noreply.github.com>
2023-11-23 19:15:18 +0100

    code optimise

--- pages/2__Recommendations.py
+++ pages/2__Recommendations.py
@@ -2,17 +2,17 @@ import streamlit as st
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-#from Home import get_top_pop
 
-#----------------------------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------
 # popularity 
-#----------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
 
 def get_top_pop(n=10):
     
-    listings_df = pd.read_csv('listings_cl.csv')
+    listings_df = pd.read_csv('listings_rec.csv')
 
-    # Define the columns to keep
+    # columns to keep
     keep_cols = ['id', 'number_of_reviews', 'review_scores_rating', 'review_scores_cleanliness', 
                  'review_scores_location','number_of_reviews_l30d','property_type']
 
@@ -30,17 +30,17 @@ def get_top_pop(n=10):
                                     & (is_superhost)
                      ].dropna(subset=['review_scores_rating'])
 
-    # Create a MinMaxScaler instance
+    # MinMaxScaler instance
     scaler = MinMaxScaler()
 
-    # Define the columns to scale
+    # columns to scale
     scale_cols = ['review_scores_rating', 'review_scores_cleanliness', 'review_scores_location',
                   'number_of_reviews','number_of_reviews_l30d']
 
-    # Apply scaling to selected columns
+    # apply scaling to selected columns
     dfp[scale_cols] = scaler.fit_transform(dfp[scale_cols])
 
-    # Define the weights for rating components
+    # define the weights for rating components
     weights = {
         'review_scores_rating': 0.50,
         'review_scores_cleanliness': 0.22,
@@ -49,18 +49,18 @@ def get_top_pop(n=10):
         'number_of_reviews_l30d':0.01,
     }
 
-    # Calculate the weighted sum of ratings
+    # calculate the weighted sum of ratings
     dfp['final_rating'] = dfp[scale_cols].dot(pd.Series(weights))
 
-    # Sort the DataFrame by the calculated 'final_rating' in descending order
+    # sort df by the calculated 'final_rating' in descending order
     dfp = dfp.sort_values(by='final_rating', ascending=False)
 
 
-    # Merge with the original DataFrame to get additional information
+    # merge with the original df to get additional information
     top_listings = dfp[['id','final_rating','property_type']].merge(listings_df[['id', 'name_0', 'number_of_reviews', 'review_scores_rating', 'description_cleaned', 'listing_url','picture_url']], on='id', how='left')
 
 
-    # Display the top 10 listings
+    # display top 10 listings
     top_n_pop = top_listings.head(n)
     
     return top_n_pop
@@ -69,11 +69,11 @@ def get_top_pop(n=10):
 
 
 
-######################################################################################################################
-#--------------------------------------------------------------------------------------------------------------------
+##########################################################################################
+#-----------------------------------------------------------------------------------------
 # RECOMMENDER APP
-#--------------------------------------------------------------------------------------------------------------------
-######################################################################################################################
+#-----------------------------------------------------------------------------------------
+##########################################################################################
 
 st.markdown(
         """
@@ -89,26 +89,20 @@ st.markdown(
         unsafe_allow_html=True,
     )
 
-#---------------------------------------------Top --------------------------------------------------------------------
+#---------------------------------------------Top ------------------------------------------
 
 
-title_1 = '<p style="font-family:sans-serif ; font-size: 42px; font-weight: 600; ">Top-rated Rentals</p>'  #color:#FF4B4B
-st.markdown(title_1, unsafe_allow_html=True)
-
-#st.caption("These rentals received high rating for location, cleanliness, and more.")
-
+title_1 = '<p style="font-family:sans-serif ; font-size: 42px; font-weight: 600; ">Top-rated Rentals</p>' 
+st.markdown(title_1, unsafe_allow_html=True)  
+#st.caption("These rentals received high rating for location, cleanliness, and more.") #color:#FF4B4B
 
 # display top 10 listings
 top_10_rentals = get_top_pop(10)
 
-
-    
 # divide page into 5 columns to display top 5
 columns = st.columns(5)
 
-
-
-# Display the listings in each column
+# display listings column wise
 for i in range(5):
     with columns[i]:
         listing = top_10_rentals.iloc[i]
@@ -116,83 +110,78 @@ for i in range(5):
         st.image(listing["picture_url"],
                      use_column_width=False,
                      width = image_width,
-                     caption='', clamp=False,
                      output_format='JPEG')
         st.write(f'**{listing["name_0"]}**')
-        st.write(f'**<p style="text-align: left;"><font color="#FF4B4B">★{listing["review_scores_rating"]}({listing["number_of_reviews"]})</font></p>**', unsafe_allow_html=True)  # ##4B853C # #FFA500
+        st.write(f'**<p style="text-align: left;"><font color="#FF4B4B">★{listing["review_scores_rating"]}({listing["number_of_reviews"]})</font></p>**', unsafe_allow_html=True)
         st.markdown('<p style="font-size: 15px; color:#FFA500;"><a style="color:#FF4B4B;" href="{0}">More on Airbnb</a></p>'.format(listing["listing_url"]), unsafe_allow_html=True)
         
 
         
-##############################
-#----------------------------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
 # popularity style wise
-#---------------------------------------------------------------------------------------------------------------------
-
-#############################
+#-----------------------------------------------------------------------------
 
 
-listings_df = pd.read_csv('listings_cl.csv')
 
-# Define the columns to keep
+listings_df = pd.read_csv('listings_rec.csv')
+
+# columns to keep
 keep_cols = ['id', 'number_of_reviews', 'review_scores_rating', 'review_scores_cleanliness', 
              'review_scores_location','number_of_reviews_l30d','property_type']
 
 # conditions
 is_available = listings_df['has_availability'] == 1
-#is_superhost = listings_df['host_is_superhost'] == 1
 host_verified = listings_df['host_identity_verified'] ==1
-#is_response = (~(listings_df['host_response_time']=='no response'))
 
 
 # Filter, select, and drop missing values in one step
 dfp = listings_df[(is_available)
                       & (host_verified)
-                       #     & (is_response)
-                        #        & (is_superhost)
                  ].dropna(subset=['review_scores_rating'])
 
-# Create a MinMaxScaler instance
+# MinMaxScaler instance
 scaler = MinMaxScaler()
 
-# Define the columns to scale
-scale_cols = ['review_scores_rating', 'review_scores_cleanliness', 'review_scores_location',
-              'number_of_reviews']
+# columns to scale
+scale_cols = ['review_scores_rating', 'review_scores_cleanliness', 'review_scores_location', 'number_of_reviews']
 
-# Apply scaling to selected columns
+# apply scaling to selected columns
 dfp[scale_cols] = scaler.fit_transform(dfp[scale_cols])
 
-# Define the weights for rating components
+# define the weights for rating components
 weights = {
-    'review_scores_rating': 0.50,
+        'review_scores_rating': 0.5,
         'review_scores_cleanliness': 0.22,
         'review_scores_location': 0.2,
         'number_of_reviews': 0.08,
        
 }
 
-# Calculate the weighted sum of ratings
+# calculate the weighted sum of ratings
 dfp['final_rating'] = dfp[scale_cols].dot(pd.Series(weights))
 
-# Sort the DataFrame by the calculated 'final_rating' in descending order
+# sort df by the calculated 'final_rating' in descending order
 dfp = dfp.sort_values(by='final_rating', ascending=False)
 
 
-# Merge with the original DataFrame to get additional information
+# merge with the original df to get additional information
 top_listings = dfp[['id','final_rating','property_type']].merge(listings_df[['id', 'name_0', 'number_of_reviews', 'review_scores_rating', 'description_cleaned', 'listing_url','picture_url']], on='id', how='left')
 
 
+# separate listings into three categories: House, Villas-Bungalows, and Boats
 
-# Separate listings into three categories: House, Villas-Bungalows, and Boats 
-house_listings = top_listings[top_listings['property_type'].isin(['Entire home','Entire vacation home','Entire guesthouse','Entire guest suite', 'Entire cottage' ])]
+house_listings = top_listings[top_listings['property_type'].isin(['Entire home','Entire vacation home','Entire guesthouse','Entire guest suite'])]
+
 villas_bunglows_listings = top_listings[top_listings['property_type'].isin(['Entire villa','Entire bungalow','Entire chalet','Private room in villa','Private room in bungalow'])]
+
 boats_listings = top_listings[top_listings['property_type'].isin(['Entire boat','Houseboat', 'Boat', 'Shared room in boat', 'Private room in boat', 'Private room in houseboat'])]
 
 
 
 
 ###########################################################################
-#----- styles
+# App part styles
 ###########################################################################
 
 
@@ -201,134 +190,46 @@ boats_listings = top_listings[top_listings['property_type'].isin(['Entire boat',
 title_2_1 = '<p style="font-family:sans-serif; ; font-size: 42px; font-weight: 600; ">Rentals for Different Styles</p>'
 st.markdown(title_2_1, unsafe_allow_html=True)
 
+# function to display rentals style wise
 
+def display_top_rentals(title, top_listings):
+    st.markdown(f'<p style="font-family:sans-serif; font-size: 32px; font-weight: 600; ">{title}</p>', unsafe_allow_html=True)
+    
+    top_10_listings = top_listings.head(10)
+
+    if len(top_10_listings) >= 5:
+        n_cols = 5
+    else:
+        n_cols = len(top_10_listings)
+
+    columns = st.columns(n_cols)
+
+    for i in range(n_cols):
+        with columns[i]:
+            listing = top_10_listings.iloc[i]
+            image_width = 140
+            st.image(listing["picture_url"],
+                     use_column_width=False,
+                     width=image_width,
+                     caption='', clamp=False,
+                     output_format='JPEG')
+            st.write(f'**{listing["name_0"]}**')
+            st.write(f'**<p style="text-align: left;"><font color="#FF4B4B">★{listing["review_scores_rating"]}({listing["number_of_reviews"]})</font></p>**', unsafe_allow_html=True)
+            st.markdown('<p style="font-size: 15px; color:#FFA500;"><a style="color:#FF4B4B;" href="{0}">More on Airbnb</a></p>'.format(listing["listing_url"]), unsafe_allow_html=True)
+            
 
 tab1, tab2, tab3 = st.tabs(["Houses", "Villas & Bungalows", "Boats"])
-#---------------------------------------------House --------------------------------------------------------------------
 
-
+#---------------------------------------------House --------------------------
 with tab1:
+    display_top_rentals("Houses", house_listings)
     
-    title_2_1 = '<p style="font-family:sans-serif; font-size: 32px; font-weight: 600; ">Houses</p>'
-    st.markdown(title_2_1, unsafe_allow_html=True)
-
-
-    # display top 10 listings houses
-    top_10_houses_listings = house_listings.head(10)
-    top_10_rentals =  top_10_houses_listings
-
-    #st.dataframe(top_10_rentals, hide_index=True)
-
-
-
-    # Display the listings in each column
-
-    if len(top_10_rentals) >= 5:
-        n_cols = 5
-    else :
-        n_cols = len(top_10_rentals)
-
-    # divide page into 5 columns to display top 5
-    columns = st.columns(n_cols)
-
-
-    for i in range(n_cols):
-        with columns[i]:
-            listing = top_10_rentals.iloc[i]
-            image_width = 140
-            st.image(listing["picture_url"],
-                         use_column_width=False,
-                         width = image_width,
-                         caption='', clamp=False,
-                         output_format='JPEG')
-            st.write(f'**{listing["name_0"]}**')
-            st.write(f'**<p style="text-align: left;"><font color="#FF4B4B">★{listing["review_scores_rating"]}({listing["number_of_reviews"]})</font></p>**', unsafe_allow_html=True)  # ##4B853C # #FFA500
-            st.markdown('<p style="font-size: 15px; color:#FFA500;"><a style="color:#FF4B4B;" href="{0}">More on Airbnb</a></p>'.format(listing["listing_url"]), unsafe_allow_html=True)
-
-
-
-#--------------------------------------------- Villas & Bunglows --------------------------------------------------------------------
-
+#--------------------------------------------- Villas & Bunglows -------------        
 with tab2:
-
-    title_2_1 = '<p style="font-family:sans-serif; font-size: 32px; font-weight: 600; ">Villas & Bunglows</p>'
-    st.markdown(title_2_1, unsafe_allow_html=True)
-
-
-    # display top 10 listings houses
-    top_10_villas_bungalows_listings = villas_bunglows_listings.head(10)
-    top_10_rentals =  top_10_villas_bungalows_listings
-
-    #st.dataframe(top_10_rentals, hide_index=True)
-
-    # Display the listings in each column
-
-    if len(top_10_rentals) >= 5:
-        n_cols = 5
-    else :
-        n_cols = len(top_10_rentals)
-
-    # divide page into 5 columns to display top 5
-    columns = st.columns(n_cols)
-
-
-
-
-    # Display the listings in each column
-    for i in range(n_cols):
-        with columns[i]:
-            listing = top_10_rentals.iloc[i]
-            image_width = 140
-            st.image(listing["picture_url"],
-                         use_column_width=False,
-                         width = image_width,
-                         caption='', clamp=False,
-                         output_format='JPEG')
-            st.write(f'**{listing["name_0"]}**')
-            st.write(f'**<p style="text-align: left;"><font color="#FF4B4B">★{listing["review_scores_rating"]}({listing["number_of_reviews"]})</font></p>**', unsafe_allow_html=True)  # ##4B853C # #FFA500
-            st.markdown('<p style="font-size: 15px; color:#FFA500;"><a style="color:#FF4B4B;" href="{0}">More on Airbnb</a></p>'.format(listing["listing_url"]), unsafe_allow_html=True)
-        
-        
-        
-#--------------------------------------------- Boats --------------------------------------------------------------------
-
+    display_top_rentals("Villas & Bungalows", villas_bunglows_listings)
+           
+#--------------------------------------------- Boats -------------------------
 with tab3:
+    display_top_rentals("Boats", boats_listings)
 
-    title_2_1 = '<p style="font-family:sans-serif; font-size: 32px; font-weight: 600; ">Boats</p>'
-    st.markdown(title_2_1, unsafe_allow_html=True)
-
-
-    # display top 10 listings houses
-    top_10_boats_listings = boats_listings.head(10)
-    top_10_rentals =  top_10_boats_listings
-
-
-
-    #st.dataframe(top_10_rentals, hide_index=True)
-
-    # Display the listings in each column
-
-    if len(top_10_rentals) >= 5:
-        n_cols = 5
-    else :
-        n_cols = len(top_10_rentals)
-
-    # divide page into 5 columns to display top 5
-    columns = st.columns(n_cols)
-
-
-
-    # Display the listings in each column
-    for i in range(n_cols):
-        with columns[i]:
-            listing = top_10_rentals.iloc[i]
-            image_width = 140
-            st.image(listing["picture_url"],
-                         use_column_width=False,
-                         width = image_width,
-                         caption='', clamp=False,
-                         output_format='JPEG')
-            st.write(f'**{listing["name_0"]}**')
-            st.write(f'**<p style="text-align: left;"><font color="#FF4B4B">★{listing["review_scores_rating"]}({listing["number_of_reviews"]})</font></p>**', unsafe_allow_html=True)  # ##4B853C # #FFA500
-            st.markdown('<p style="font-size: 15px; color:#FFA500;"><a style="color:#FF4B4B;" href="{0}">More on Airbnb</a></p>'.format(listing["listing_url"]), unsafe_allow_html=True)
-        
+ 
--- pages/3__Personalised Place.py
+++ pages/3__Personalised Place.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
-from Home import content_rec
+
 
 # load data
-listings_df = pd.read_csv('listings_cl.csv')
+listings_df = pd.read_csv('listings_rec.csv')
 
 #-----------------------------------------------------------------------
 # Content based
@@ -41,37 +41,57 @@ listings_df['desc_proc'] = listings_df['desc_proc'].apply(preprocess_text)
 listings_df = listings_df[(listings_df['has_availability'] == 1)]
 
 def content_rec(desired_neighbourhood,desired_number_person,low_price,high_price,input_text):
+    
     # Step 1: Filter Listings
     filtered_listings = listings_df[(listings_df['accommodates'] == desired_number_person) &
                                    (listings_df['neighbourhood_cleansed'] == desired_neighbourhood) &
                                    (listings_df['price'] >= low_price) &
                                    (listings_df['price'] <= high_price)]
-    input_text = "Apartment with balcony, cups, dishes, kitchen, coffee table, for 2 person, 3rd or above floor, sunny, wifi, tv"
-    
-    # Step 2: Calculate TF-IDF for Input Text and Filtered Listings
-    tfidf_vectorizer = TfidfVectorizer(
-        #max_df=1.0, min_df=1
-    )
+
+    # Step 2: take user input and clean it
+    def clean_text(input_text):
+
+        # Remove punctuation
+        translator = str.maketrans('', '', string.punctuation)
+        cleaned_text = input_text.translate(translator)
+
+        # Convert to lowercase and strip leading/trailing spaces
+        cleaned_text = cleaned_text.lower().strip()
+
+        return cleaned_text
+
+    input_text = 'Apartment with bal%cony, (cups), dishesß, kitchen, öl, coffee table,for 2 person, 3rd or above floor, sunny, wifi, tv'
+    cleaned_text = clean_text(input_text)
+
+    # Step 3: Calculate TF-IDF for Input Text and Filtered Listings
+    tfidf_vectorizer = TfidfVectorizer(analyzer = 'word') #, max_df=0.95, min_df=0
     tfidf_matrix = tfidf_vectorizer.fit_transform(filtered_listings['desc_proc'])
-    input_tfidf = tfidf_vectorizer.transform([input_text])
+    input_tfidf = tfidf_vectorizer.transform([cleaned_text])
 
-    # Step 3: Calculate Cosine Similarity
-    cosine_sim = cosine_similarity(input_tfidf, tfidf_matrix)
+    # Step 4: Calculate Cosine Similarity
+    cosine_sim = cosine_similarity(input_tfidf, tfidf_matrix,)
 
-    # Step 4: Sort and Retrieve Top Similar Listings
-    similar_listings_indices = cosine_sim[0].argsort()[::-1]  # Sort in descending order
+    # Create a DataFrame with the cosine similarity scores
+    cosine_sim_df = pd.DataFrame(cosine_sim[0], index=filtered_listings.id)
+
+    # Step 5: Sort and Retrieve Top Similar Listings
+    cosine_sim_df = cosine_sim_df.sort_values(by=0,ascending=False).reset_index()
 
     # columns
-    display_columns = ['id','name_0','number_of_reviews','description_cleaned','accommodates','review_scores_rating','neighbourhood_cleansed','price','listing_url','picture_url']
-    top_similar_listings = filtered_listings[display_columns].iloc[similar_listings_indices].head(10)
+    display_columns =['id','name_0','number_of_reviews','description_cleaned','accommodates','review_scores_rating',
+                      'neighbourhood_cleansed','price','listing_url','picture_url']
+
+    #    top_similar_listings = filtered_listings[display_columns].iloc[similar_listings_indices].head(10)
+    top_similar_listings = cosine_sim_df.merge(filtered_listings[display_columns],how='left')
+
 
     return top_similar_listings
 
-######################################################################################################################
-#--------------------------------------------------------------------------------------------------------------------
+####################################################################################
+#-----------------------------------------------------------------------------------
 # RECOMMENDER APP
-#--------------------------------------------------------------------------------------------------------------------
-######################################################################################################################
+#-----------------------------------------------------------------------------------
+#####################################################################################
 
 
 
@@ -89,9 +109,9 @@ st.markdown(
         unsafe_allow_html=True,
     )
 
-#----------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------
 # Content based
-#----------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------
 
 
 #st.header("Personalize Your Stay Here!")
@@ -101,47 +121,35 @@ title_2 = '<p style="font-family:sans-serif; color:#FF4B4B; font-size: 42px; fon
 st.markdown(title_2, unsafe_allow_html=True)
 
 
-# formatting, create 3 columns
-#col1, col2  = st.columns([1,1])
-
-# header
-# add image
-#col2.image("image-2.jpg", width = 410, caption = "Photo Credit Patrick Reichboth via Unsplash. Location: Prenzlauer Berg, Berlin, Germany")
-
 
 subtitle1 = '<p style="font-size: 20px;">Using the dropdown menus, sliders, and by explaining your preference get recommendations.</p>'
 st.markdown(subtitle1, unsafe_allow_html = True)
-#col1.markdown(subtitle1, unsafe_allow_html = True)
 
 
 # GET USER INPUTS #
-# ----------------------------------------------- #
+
 unique_neighborhood = sorted(listings_df['neighbourhood_cleansed'].unique())
-neighborhood = st.selectbox('Location:',(unique_neighborhood))
-
 unique_accommodates = sorted(listings_df['accommodates'].unique())
-accommodation = st.slider('Number of Guests:',listings_df['accommodates'].min(),listings_df['accommodates'].max(),value=2 )
 
-# Create two input boxes for minimum and maximum prices side by side
-
+neighborhood = st.selectbox('Location:',(unique_neighborhood))
+accommodation = st.slider('Number of Guests:', listings_df['accommodates'].min(), listings_df['accommodates'].max(), value=1)
 col1, col2 = st.columns(2)
-
 with col1:
     min_price = st.number_input("Minimum Price", min_value=0, max_value=800, value=100)
-
 with col2:
     max_price = st.number_input("Maximum Price", min_value=0, max_value=800, value=200)
-
 user_input = st.text_area("Describe your requirements",max_chars=200)
 
-# call fun to get similar listings
-similar_rentals = content_rec(desired_neighbourhood = neighborhood ,
+
+
+if st.button("Show me recommendations!"):
+    
+    # call fun to get similar listings
+    similar_rentals = content_rec(desired_neighbourhood = neighborhood ,
                                   desired_number_person = accommodation ,
                                   low_price = min_price,
                                   high_price = max_price,
                                   input_text = user_input)
-
-if st.button("Show me recommendations!"):
 
     if len(similar_rentals) ==0:
         st.write('Soory We can not find a good place for you.')
